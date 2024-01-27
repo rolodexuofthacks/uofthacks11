9caa101f43e7ec4d567a2148aa5e735282ad98c0
mahzabinrashid <mahzabinrashidfariha@gmail.com>
2024-01-27 03:42:19 -0500

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Card from "./ui-components/Card/card";
 import Timeline from "./timeline/timeline";
 
 function App() {
   return (
     <div className="App">
-      <Card />
-      {/* move timeline to wherever required */}
-      <Timeline /> 
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Card />} />
+          <Route path="/storyline" element={<Timeline />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- frontend/src/ui-components/Card/CardContent.js
+++ frontend/src/ui-components/Card/CardContent.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from "react";
 import "./card.css";
+import { useNavigate } from "react-router-dom";
 
 const CardContent = ({ name, image }) => {
-  return (
-    <div className='card'>
-      <img src={image} alt={name} />
-      <div className='description'>
-        <h2>{name}</h2>
-        <button className='btn'>storyline</button>
+  const navigate = useNavigate();
 
+  function handleClick() {
+    navigate("/storyline");
+  }
+
+  return (
+    <div className="card">
+      <img src={image} alt={name} />
+      <div className="description">
+        <h2>{name}</h2>
+        <button onClick={handleClick} className="btn">
+          View Storyline
+        </button>
       </div>
-      
     </div>
   );
 };
