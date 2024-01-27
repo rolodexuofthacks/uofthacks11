21c0199d93665c973612d6ee8f08eb6d1dbff8b6
akerrules <addrido@gmail.com>
2024-01-26 23:00:08 -0500

    added an example to fetch the data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  //example to fetch
+  const [text, setText] = useState("");
+
+  // State to handle loading status
+  const [isLoading, setIsLoading] = useState(true);
+  // State to handle any errors
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/hello")
+      .then((res) => res.json())
+      .then((data) => {
+        setText(data);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
