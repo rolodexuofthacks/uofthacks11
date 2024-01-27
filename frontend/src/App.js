11ddd3bdefb72b8f52b913b2473935ec0cb9c381
mahzabinrashid <mahzabinrashidfariha@gmail.com>
2024-01-27 01:56:35 -0500

    timeline component done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
-import "./App.css";
 import Card from "./ui-components/Card/card";
+import Timeline from "./timeline/timeline";
 
 function App() {
   return (
     <div className="App">
       <Card />
+      {/* move timeline to wherever required */}
+      <Timeline /> 
+
     </div>
   );
 }
